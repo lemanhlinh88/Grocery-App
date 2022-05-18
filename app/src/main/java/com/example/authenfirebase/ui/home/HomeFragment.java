40a5220bb9363bd6b42075a7663bde359918b35d
namdv01 <khonghoatay@gmail.com>
2022-05-18 11:51:04 +0700

    add full home

--- app/src/main/java/com/example/authenfirebase/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/authenfirebase/ui/home/HomeFragment.java
@@ -15,9 +15,13 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.authenfirebase.R;
+import com.example.authenfirebase.adapters.HomeAdapter;
 import com.example.authenfirebase.adapters.PopularAdapters;
+import com.example.authenfirebase.adapters.RecommendedAdapter;
 import com.example.authenfirebase.databinding.FragmentHomeBinding;
+import com.example.authenfirebase.models.HomeCategory;
 import com.example.authenfirebase.models.PopularModel;
+import com.example.authenfirebase.models.RecommendedModel;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -32,8 +36,17 @@ public class HomeFragment extends Fragment {
     //popular items
     List<PopularModel> popularModelList;
     PopularAdapters popularAdapters;
-    RecyclerView popularRec;
+
+    RecyclerView popularRec,homeCatRec,recommendedRec;
     FirebaseFirestore db;
+
+    //home category
+    List<HomeCategory> categoryList;
+    HomeAdapter homeAdapter;
+
+    //recommended
+    List<RecommendedModel> recommendedModelList;
+    RecommendedAdapter recommendedAdapter;
 
     private FragmentHomeBinding binding;
 
@@ -44,6 +57,8 @@ public class HomeFragment extends Fragment {
         View root = binding.getRoot();
         db = FirebaseFirestore.getInstance();
         popularRec = root.findViewById(com.example.authenfirebase.R.id.popular_recycle);
+        homeCatRec = root.findViewById(com.example.authenfirebase.R.id.explore_recycle);
+        recommendedRec = root.findViewById(com.example.authenfirebase.R.id.recommended_recycle);
 
         //popular items
         popularRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
@@ -61,6 +76,52 @@ public class HomeFragment extends Fragment {
                                 PopularModel popularModel = document.toObject(PopularModel.class);
                                 popularModelList.add(popularModel);
                                 popularAdapters.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(),"Có lỗi xảy ra",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        //home category
+        homeCatRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        categoryList = new ArrayList<>();
+        homeAdapter = new HomeAdapter(getActivity(),categoryList);
+        homeCatRec.setAdapter(homeAdapter);
+
+        db.collection("HomeCategory")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                HomeCategory homeCategory = document.toObject(HomeCategory.class);
+                                categoryList.add(homeCategory);
+                                homeAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(),"Có lỗi xảy ra",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        //recommended
+        recommendedRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        recommendedModelList = new ArrayList<>();
+        recommendedAdapter = new RecommendedAdapter(getActivity(),recommendedModelList);
+        recommendedRec.setAdapter(recommendedAdapter);
+
+        db.collection("Recommended")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                RecommendedModel recommendedModel = document.toObject(RecommendedModel.class);
+                                recommendedModelList.add(recommendedModel);
+                                recommendedAdapter.notifyDataSetChanged();
                             }
                         } else {
                             Toast.makeText(getActivity(),"Có lỗi xảy ra",Toast.LENGTH_SHORT).show();
