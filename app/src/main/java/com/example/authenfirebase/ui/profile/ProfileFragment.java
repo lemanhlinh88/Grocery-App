86831792f71356a045ff9353060f79b0ff4cb660
lemanhlinh88 <lemanhlinh0808@gmail.com>
2022-05-21 15:43:45 +0700

    profile update

--- app/src/main/java/com/example/authenfirebase/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/authenfirebase/ui/profile/ProfileFragment.java
@@ -1,31 +1,84 @@
 package com.example.authenfirebase.ui.profile;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.authenfirebase.R;
 import com.example.authenfirebase.databinding.FragmentProfileBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.net.URI;
+import java.nio.file.FileStore;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileFragment extends Fragment {
+    CircleImageView profileImage;
+    EditText name, email, phone, address;
+    Button update;
+
+    FirebaseStorage storage;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+
 
     private FragmentProfileBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-//        GalleryViewModel galleryViewModel =
-//                new ViewModelProvider(this).get(GalleryViewModel.class);
 
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-//        final TextView textView = binding.textGallery;
-//        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        auth = FirebaseAuth.getInstance();
+        storage = FirebaseStorage.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        profileImage = root.findViewById(R.id.profile_img);
+        name = root.findViewById(R.id.profile_name);
+        email = root.findViewById(R.id.profile_email);
+        phone = root.findViewById(R.id.profile_phone);
+        address = root.findViewById(R.id.profile_address);
+        update = root.findViewById(R.id.profile_btn_update);
+
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, 33);
+            }
+        });
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateUserProfile();
+            }
+        });
+
+
+
         return root;
     }
 
@@ -33,5 +86,40 @@ public class ProfileFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void updateUserProfile(){
+
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(data.getData() != null){
+            Uri profileUri = data.getData();
+            profileImage.setImageURI(profileUri);
+
+            final StorageReference reference = storage.getReference().child("profile_picture")
+                    .child(FirebaseAuth.getInstance().getUid());
+
+            reference.putFile(profileUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(getContext(), "Cập nhật thông tin thành công", Toast.LENGTH_SHORT).show();
+
+                    reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            database.getReference().child("Users").child(FirebaseAuth.getInstance().getUid())
+                                    .child("profileImg").setValue(uri.toString());
+                            Toast.makeText(getContext(), "Cập nhật thông tin thành công", Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+                }
+            });
+        }
     }
 }
